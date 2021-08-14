dd99b9d458098ef656d8ab20859d0c184b00ea51
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-08-15 00:57:45 +0530

    Add files via upload

---
+++ SOLID_Principles_S/SOLID_Principles_S/GarageStation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles_S
+{
+
+    public class GarageStation
+    {
+        IGarageUtility _garageUtil;
+
+        public GarageStation(IGarageUtility garageUtil)
+        {
+            this._garageUtil = garageUtil;
+        }
+        public void OpenForService()
+        {
+            _garageUtil.OpenGate();
+        }
+        public void DoService()
+        {
+            //Check if service station is opened and then
+            //finish the vehicle service
+        }
+        public void CloseGarage()
+        {
+            _garageUtil.CloseGate();
+        }
+    }
+    public class GarageStationUtility : IGarageUtility
+    {
+        public void OpenGate()
+        {
+            //Open the Garage for service
+        }
+
+        public void CloseGate()
+        {
+            //Close the Garage functionlity
+        }
+    }
+
+    public interface IGarageUtility
+    {
+        void OpenGate();
+        void CloseGate();
+    }
+}
