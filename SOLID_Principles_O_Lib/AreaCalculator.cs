6000432d11c411c3e619a761b865d4f74ff13691
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-08-15 14:01:07 +0530

    Add files via upload

---
+++ SOLID_Principles_O_Lib/AreaCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles_O_Lib
+{
+    public interface Shape
+    {
+        double Area();
+    }
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Area()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public double Area()
+        {
+            return Radius * Radius * Math.PI;
+        }
+    }
+
+    public class AreaCalculator
+    {
+        /*
+        public double TotalArea(object[] arrObjects)
+        {
+            double area = 0;
+            Rectangle objRectangle;
+            Circle objCircle;
+            foreach (var obj in arrObjects)
+            {
+                if (obj is Rectangle)
+                {
+                    area += obj.Height * obj.Width;
+                }
+                else
+                {
+                    objCircle = (Circle)obj;
+                    area += objCircle.Radius * objCircle.Radius * Math.PI;
+                }
+            }
+            return area;
+        }
+        */
+
+        public double TotalArea(Shape[] arrShapes)
+        {
+            double area = 0;
+            foreach (var objShape in arrShapes)
+            {
+                area += objShape.Area();
+            }
+            return area;
+        }
+
+        //class AreaCalculator is now closed for modification and open for extension
+
+    }
+}
