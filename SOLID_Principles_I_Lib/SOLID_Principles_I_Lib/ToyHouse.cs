ca3a01d5b71e304afecd4c60c69d441142847169
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-08-15 13:53:43 +0530

    Add files via upload

---
+++ SOLID_Principles_I_Lib/SOLID_Principles_I_Lib/ToyHouse.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles_I_Lib
+{
+    public interface IToy
+    {
+        void SetPrice(double price);
+        void SetColor(String color);
+        //void Move();
+        //void Fly();
+    }
+
+    public interface IMove
+    {
+        //void Move();
+        //void Fly();
+    }
+    class ToyHouse : IToy
+    {
+        double price;
+        String color;
+
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+        public void SetColor(String color)
+        {
+            this.color = color;
+        }
+        //public void Move()
+        //{
+        //    throw new Exception("Not allowed");
+        //}
+        //public void Fly()
+        //{
+        //    throw new Exception("Not allowed");
+        //}
+    }
+
+    class AirPlane : IToy, IMove
+    {
+        double price;
+        String color;
+
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+        public void SetColor(String color)
+        {
+            this.color = color;
+        }
+
+        public void Move()
+        {
+            //move
+        }
+        public void Fly()
+        {
+            //fly
+        }
+
+    }
+}
