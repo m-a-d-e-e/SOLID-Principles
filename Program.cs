1f10e133ce3ec1093380d2dc3634e6a98907317d
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-08-15 14:02:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOLID_Principles_O_Lib;
+
+namespace SOLID_O_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            r1.Height = 1.2;
+            r1.Width = 2.2;
+
+            Rectangle r2 = new Rectangle();
+            r1.Height = 4.9;
+            r1.Width = 2.3;
+
+            Circle c1 = new Circle();
+            c1.Radius = 2.0;
+
+            Shape[] shapes = { r1, r2, c1 };
+
+            AreaCalculator ac = new AreaCalculator();
+            Console.WriteLine("Total area of all shapes : " + ac.TotalArea(shapes));
+
+            Console.ReadLine();
+        }
+    }
+}
