32c8091c90fd0bed4cea89a1e953a136245d9623
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-08-15 13:54:07 +0530

    Add files via upload

---
+++ SOLID_Principles_D_Lib/SOLID_Principles_D_Lib/Notification.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles_D_Lib
+{
+    public interface IMessenger
+    {
+        void SendMessage();
+    }
+    public class Email : IMessenger
+    {
+        public void SendMessage()
+        {
+            // code to send email
+        }
+    }
+
+    public class SMS : IMessenger
+    {
+        public void SendMessage()
+        {
+            // code to send SMS
+        }
+    }
+
+
+    public class Notification
+    {
+        private IMessenger _iMessenger;
+        public Notification(IMessenger pMessenger)
+        {
+            _iMessenger = pMessenger;
+        }
+        public void DoNotify()
+        {
+            _iMessenger.SendMessage();
+        }
+    }
+}
